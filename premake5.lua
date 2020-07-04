d7dac7dfaed3d255cfa2ccbe4cdb898171da861d
tommysr <47206288+tommysr@users.noreply.github.com>
2020-07-04 19:04:06 +0200

    added premake control

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Aitomus"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Aitomus"
+	location "Aitomus"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"AIT_PLATFORM_WINDOWS",
+			"AIT_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "AIT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "AIT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "AIT_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Aitomus/vendor/spdlog/include",
+		"Aitomus/src"
+	}
+
+	links
+	{
+		"Aitomus"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"AIT_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "AIT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "AIT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "AIT_DIST"
+		optimize "On"
